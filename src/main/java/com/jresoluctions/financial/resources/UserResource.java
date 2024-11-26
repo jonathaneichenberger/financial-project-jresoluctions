abbb6872bcc48f820abf4d101e29f85b2e33cc66
Jonathan Eichenberger <djohnni@hotmail.com>
2024-11-26 11:30:27 -0300

    Adicionada a camada Service e adicionado as anotações para registro dos componentes

--- src/main/java/com/jresoluctions/financial/resources/UserResource.java
+++ src/main/java/com/jresoluctions/financial/resources/UserResource.java
@@ -1,18 +1,33 @@
 package com.jresoluctions.financial.resources;
 
 import com.jresoluctions.financial.entities.User;
+import com.jresoluctions.financial.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "maria", "maria@gmail.com", "(47)99332-1232", "tata1212");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }

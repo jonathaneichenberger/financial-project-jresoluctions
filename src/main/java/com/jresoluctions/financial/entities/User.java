b44e09daee6c9702d37f0197049d992d960935d6
Jonathan Eichenberger <djohnni@hotmail.com>
2024-11-26 15:54:31 -0300

    Adicionado a classe Order e feito o relacionamento User1-->*Order

--- src/main/java/com/jresoluctions/financial/entities/User.java
+++ src/main/java/com/jresoluctions/financial/entities/User.java
@@ -1,7 +1,10 @@
 package com.jresoluctions.financial.entities;
 
 import jakarta.persistence.*;
+
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -14,6 +17,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -64,6 +70,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override

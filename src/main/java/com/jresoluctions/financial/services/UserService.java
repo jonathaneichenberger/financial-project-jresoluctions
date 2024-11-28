3d0a18b453959a1be994ea65a6f8e755dd8174f7
Jonathan Eichenberger <djohnni@hotmail.com>
2024-11-28 08:04:04 -0300

    Adicionando Tratamento de Exceções personalizados

--- src/main/java/com/jresoluctions/financial/services/UserService.java
+++ src/main/java/com/jresoluctions/financial/services/UserService.java
@@ -3,6 +3,7 @@ package com.jresoluctions.financial.services;
 
 import com.jresoluctions.financial.entities.User;
 import com.jresoluctions.financial.repositories.UserRepository;
+import com.jresoluctions.financial.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
